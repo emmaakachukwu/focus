cb5281af104d8c5695583b3fa174ed2176354e2c
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2021-01-04 09:08:23 +0100

    add n rm from cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,17 +1,33 @@
 let cart = get_cart();
 
 function add_to_cart(id, name, price, image, quantity = 1) {
-    let product = {
-        id,
-        name,
-        price,
-        image,
-        quantity
+    check = check_product(id)
+    console.log(check)
+    if (!check) {
+        let product = {
+            id,
+            name,
+            price,
+            image,
+            quantity
+        }
+        cart.push(product)
+    } else {
+        remove_from_cart(id)
     }
-    cart.push(product)
     localStorage.setItem('cart', JSON.stringify(cart))
 
     modify_cart(cart)
+    switch_text(id, !check)
+}
+
+function remove_from_cart(id) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id == id) {
+            cart.splice(i, 1)
+            break
+        }
+    }
 }
 
 function get_cart() {
@@ -26,8 +42,7 @@ function modify_cart(cart) {
     let style = document.createElement('style')
 
     if (style.styleSheet) {
-        style.styleSheet.cssText = css
-    } else {
+ons    } else {
         style.appendChild(document.createTextNode(css))
     }
     document.getElementsByTagName('head')[0].appendChild(style)
@@ -38,10 +53,25 @@ function check_products_in_cart(products) {
     products.forEach(e => {
         for (let i = 0; i < cart.length; i++) {
             if (cart[i].id == e.id) {
-                let add = document.querySelector('#add-' + e.id)
-                add.innerText = 'Remove from cart'
+                switch_text(cart[i].id)
                 break
             }
         }
     })
+}
+
+function check_product(id) {
+    let in_cart = false
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id == id) {
+            in_cart = true
+            break
+        }
+    }
+    return in_cart
+}
+
+function switch_text(id, in_cart = true) {
+    let add = document.querySelector('#add-' + id)
+    add.innerText = in_cart ? 'Remove from cart' : 'Add to cart'
 }
