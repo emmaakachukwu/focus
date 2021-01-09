eed28ff439c69b3bf71e1f593ff456e297d6c843
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2021-01-09 19:36:32 +0100

    flutterwave integration

--- checkout.php
+++ checkout.php
@@ -2,6 +2,7 @@
 $title = $header_title = "Checkout";
 require_once "./components/header.php";
 require_once "./lib/auth.php";
+require_once "./utils.php";
 
 $cart = json_decode($_COOKIE['cart']);
 $total = 0;
@@ -28,12 +29,49 @@ foreach ($cart as $c) {
             <div class="card p-4 mb-30">
                 <h6>Pay online now</h6>
                 <form action="./forms/checkout.php" method="post">
+                    <script src="https://checkout.flutterwave.com/v3.js"></script>
                     <input type="hidden" name='type' value='2'>
-                    <button class="btn btn-primary">Continue</button>
+                    <button type="button" class="btn btn-primary" onclick="makePayment('<?php echo $user->email?>', '<?php echo $user->phone?>', '<?php echo $user->fname?>', '<?php echo $total ?>')">Continue</button>
                 </form>
             </div>
         </div>
     </section>
 </main>
 
-<?php include_once "./components/footer.php";
+<?php include_once "./components/footer.php" ?>
+
+<script>
+  function makePayment(email, phone, name, amount) {
+    console.log(new Date().getTime());
+    FlutterwaveCheckout({
+      public_key: "<?php echo PUBLIC_KEY ?>",
+      tx_ref: new Date().getTime(),
+      amount: parseInt(amount),
+      currency: "NGN",
+      country: "NG",
+      payment_options: "card, mobilemoneyghana, ussd",
+      redirect_url: // specified redirect URL
+        "http://127.0.0.1/focus/forms/checkout.php?type=2",
+      meta: {
+        consumer_id: 23,
+        consumer_mac: "92a3-912ba-1192a",
+      },
+      customer: {
+        email: email,
+        phone_number: phone,
+        name: name,
+      },
+      callback: function (data) {
+        console.log(data);
+      },
+      onclose: function() {
+        // close modal
+      },
+      customizations: {
+        title: "My store",
+        description: "Payment for items in cart",
+        logo: "https://assets.piedpiper.com/logo.png",
+      },
+    });
+  }
+</script>
--- forms/checkout.php
+++ forms/checkout.php
@@ -1,5 +1,4 @@
 <?php
-use Yabacon\Paystack;
 
 require_once "./../lib/config.php";
 
@@ -9,10 +8,17 @@ if ( !is_logged_in() ) {
     $uuid = $_SESSION['uuid'];
 }
 
-if ( !isset($post['type']) || empty($post['type']) )
-    array_push($errors, 'something went wrong; retry..');
-else
+if ( isset($post['type']) && !empty($post['type']) )
     $type = $post['type'];
+else if ( isset($_GET['type']) && !empty($_GET['type']) )
+    $type = $_GET['type'];
+else
+    array_push($errors, 'something went wrong; retry..');
+
+if ( isset($_GET['transaction_id']) && !empty($_GET['transaction_id']) )
+    $txn_id = $_GET['transaction_id'];
+else 
+    array_push($errors, 'missing transaction url');
 
 check_errors($errors);
 
@@ -33,60 +39,47 @@ if ( $result->num_rows ) {
 $hash = md5($uuid . time() . '...');
 $order_id = substr($hash, 0, 5) . substr($hash, -5, 5);
 
-if ( $type == '1' ) {
+if ( $type == '1' )
     if ( $user->balance < $total )
         array_push($errors, 'Your wallet balance is too low to place this order', 'Use the online payment option or go to deposit to top up your balance');
+    else
+        $order_id = substr($hash, 0, 5) . substr($hash, -5, 5);
+else if ( $type == '2' )
+    if ( !isset($_GET['status']) || $_GET['status'] !== 'successful' )
+        array_push($errors, 'Payment not successful');
+    else
+        $order_id = $txn_id;
 
-    check_errors($errors);
+check_errors($errors);
 
-    $at = date('Y-m-d H:i:s');
-    $ok = false;
-    foreach ( $cart as $c ) {
-        try {
-            $sql = $link->prepare("INSERT INTO `orders`(order_id, user_id, product_id, quantity, paid, created_at, updated_at) VALUES (?, ?, ?, ?, true, ?, ?)");
-            $sql->bind_param("ssssss", $order_id, $user->id, $c->id, $c->quantity, $at, $at);
-            if ( $sql->execute() ) {
-                $sql->close();
-                $ok = true;
-            } else {
-                $ok = false;
-            }
+$at = date('Y-m-d H:i:s');
+$ok = false;
+foreach ( $cart as $c ) {
+    try {
+        $sql = $link->prepare("INSERT INTO `orders`(order_id, user_id, product_id, quantity, paid, created_at, updated_at) VALUES (?, ?, ?, ?, true, ?, ?)");
+        $sql->bind_param("ssssss", $order_id, $user->id, $c->id, $c->quantity, $at, $at);
+        if ( $sql->execute() ) {
             $sql->close();
-        } catch ( \Exception $err ) {
+            $ok = true;
+        } else {
             $ok = false;
         }
+        $sql->close();
+    } catch ( \Exception $err ) {
+        $ok = false;
     }
+}
 
-    if ( $ok ) {
-        $sql = $link->prepare("UPDATE `users` SET balance=balance-?, updated_at=? WHERE id=?");
-        $sql->bind_param("sss", $total, $at, $user->id);
-        if ( $sql->execute() ) {
-            clear_cookies('cart');
+if ( $ok ) {
+    $sql = $link->prepare("UPDATE `users` SET balance=balance-?, updated_at=? WHERE id=?");
+    $sql->bind_param("sss", $total, $at, $user->id);
+    if ( $sql->execute() ) {
+        clear_cookies('cart');
 
-            $_GET['ccart'] = 'true';
-            $_SESSION['success'] = ['Order has been saved'];
-            on_success('index');
-        }
+        $_GET['ccart'] = 'true';
+        $_SESSION['success'] = ['Order has been saved'];
+        on_success('index');
     }
-} else if ( $type == '2' ) {
-    $paystack = new Yabacon\Paystack('sk_test_8d43572dd1a9f390f7058073c7e8f85e2c24048e');
-
-    try
-    {
-      $tranx = $paystack->transaction->initialize([
-        'amount'=>$total * 100,       // in kobo
-        'email'=>$user->email,         // unique to customers
-        'reference'=>$order_id, // unique to transactions
-      ]);
-    } catch(\Yabacon\Paystack\Exception\ApiException $e){
-      print_r($e->getResponseObject());
-      die($e->getMessage());
-    }
-
-    save_last_transaction_reference($tranx->data->reference);
-
-    // redirect to page so User can pay
-    header('Location: ' . $tranx->data->authorization_url);
 }
 
 array_push($errors, 'Something went wrong; retry');
